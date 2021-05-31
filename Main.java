2a40453265d1f906a660804602413861a696013b
Gustavo Ullmann <gustavoullmann@gmail.com>
2021-05-31 20:42:23 -0300

    Modificado para testes

--- Main.java
+++ Main.java
@@ -3,9 +3,8 @@ public class Main {
 
     static Tree AVL_TREE = new Tree();                                      //Instancia uma nova árvore ao executar o programa
     public static void main(String[] args) {
-        Menu.menu();
-        
+        //Menu.menu();
+        CSV_Reader.CSV_importer();
+
     }
 }
-
-//teste
