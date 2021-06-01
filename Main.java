d44fa52805f6d3815c2f2bbe73a8531a98a5f099
Gustavo Ullmann <gustavoullmann@gmail.com>
2021-06-01 12:36:09 -0300

    Apenas alterção da chamada de método da classe que teve nome alterado

--- Main.java
+++ Main.java
@@ -4,7 +4,7 @@ public class Main {
     static Tree AVL_TREE = new Tree();                                      //Instancia uma nova árvore ao executar o programa
     public static void main(String[] args) {
         //Menu.menu();
-        CSV_Reader.CSV_importer();
+        File_Importer.CSV_reader();
 
     }
 }
