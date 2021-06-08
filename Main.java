43495482e4704fca71a74aa8b73bf15e45796bfc
Gustavo Ullmann <gustavoullmann@gmail.com>
2021-06-08 11:31:15 -0300

    Modificações para testes e inclusão da instância de AVL_TREE (ver comentário de linha)

--- Main.java
+++ Main.java
@@ -2,19 +2,21 @@ import java.util.Date;
 
 public class Main {
 
+    static Tree<Integer> AVL_TREE = new Tree<Integer>();   //TODO: remover essa instância: foi colocada só para eliminar os erros da classe Menu (métodos do trabalho antigo)
     static Tree<Long> CPF_TREE = new Tree<Long>();   
     static Tree<String> NOME_TREE = new Tree<String>(); 
     static Tree<Date> DATANASC_TREE = new Tree<Date>();
     
     public static void main(String[] args) {
+        Menu.menu();
+        //File_Importer.CSV_reader("/home/gustavo/Área de Trabalho/cadastro_pessoas_random.csv"); //TODO: remover esse teste   
+
+        // CPF_TREE.printTree(CPF_TREE.getRoot(), 0);                  //TODO: remover esse teste  
+        // System.out.println("\n\n" + "##############################################################################");  //TODO: remover esse teste  
+        // NOME_TREE.printTree(NOME_TREE.getRoot(), 0);                //TODO: remover esse teste  
+        // System.out.println("\n\n" + "##############################################################################");  //TODO: remover esse teste  
+        // DATANASC_TREE.printTree(DATANASC_TREE.getRoot(), 0);        //TODO: remover esse teste  
+
         //Menu.menu();
-        File_Importer.CSV_reader(); //TODO: remover esse teste
-
-        CPF_TREE.inOrderTraversal(CPF_TREE.getRoot());
-        System.out.println();
-        NOME_TREE.inOrderTraversal(NOME_TREE.getRoot());
-        System.out.println();
-        DATANASC_TREE.inOrderTraversal(DATANASC_TREE.getRoot());
-
     }
 }
