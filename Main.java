26b8595192a1f211b2bcf252912108c0feb10d1a
Gustavo Ullmann <gustavoullmann@gmail.com>
2021-06-08 21:40:30 -0300

    Modificações para testes

--- Main.java
+++ Main.java
@@ -1,3 +1,4 @@
+import java.time.LocalDate;
 import java.util.Date;
 
 public class Main {
@@ -5,11 +6,24 @@ public class Main {
     static Tree<Integer> AVL_TREE = new Tree<Integer>();   //TODO: remover essa instância: foi colocada só para eliminar os erros da classe Menu (métodos do trabalho antigo)
     static Tree<Long> CPF_TREE = new Tree<Long>();   
     static Tree<String> NOME_TREE = new Tree<String>(); 
-    static Tree<Date> DATANASC_TREE = new Tree<Date>();
+    static Tree<LocalDate> DATANASC_TREE = new Tree<LocalDate>();
     
     public static void main(String[] args) {
-        Menu.menu();
-        //File_Importer.CSV_reader("/home/gustavo/Área de Trabalho/cadastro_pessoas_random.csv"); //TODO: remover esse teste   
+        
+        //Menu.menu();
+        
+        File_Importer.CSV_reader("cadastro_pessoas.csv"); //TODO: remover esse teste   
+
+
+        DATANASC_TREE.printTree(DATANASC_TREE.getRoot(), 0);
+
+        String inputMinDate = "01/01/2001";
+        LocalDate minDate = Menu.readDataNasc(inputMinDate);
+        
+        String inputMaxDate = "07/08/2007";
+        LocalDate maxDate = Menu.readDataNasc(inputMaxDate);
+
+        DATANASC_TREE.searchNode(minDate).getPessoa().printInformations();
 
         // CPF_TREE.printTree(CPF_TREE.getRoot(), 0);                  //TODO: remover esse teste  
         // System.out.println("\n\n" + "##############################################################################");  //TODO: remover esse teste  
