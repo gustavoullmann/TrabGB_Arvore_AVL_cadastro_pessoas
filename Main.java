e165cc8a858b3a4e9d38747d7fc33a28e13ba46d
Gustavo Ullmann <gustavoullmann@gmail.com>
2021-06-12 21:16:36 -0300

    Exclusão de instanciação de AVL_TREE

--- Main.java
+++ Main.java
@@ -2,7 +2,6 @@ import java.time.LocalDate;
 
 public class Main {
 
-    static Tree<Integer> AVL_TREE = new Tree<Integer>();   //TODO: remover essa instância: foi colocada só para eliminar os erros da classe Menu (métodos do trabalho antigo)
     static Tree<Long> CPF_TREE = new Tree<Long>();   
     static Tree<String> NOME_TREE = new Tree<String>(); 
     static Tree<LocalDate> DATANASC_TREE = new Tree<LocalDate>();
